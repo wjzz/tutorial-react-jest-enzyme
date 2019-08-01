af49e6ca8532d7a7cc01399134c6412b5dd5a6bb
Wojciech Jedynak <w.jedynak@univapay.com>
2019-08-01 14:53:27 +0900

    Final step before CI

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
+import { shallow, mount, render } from "enzyme";
+import renderer from "react-test-renderer";
+
+it("renders without crashing", () => {
+  const div = document.createElement("div");
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it("works fine with enzyme", () => {
+  const wrapper = shallow(<App />);
+
+  expect(wrapper.text()).toContain("Learn React");
+});
+
+test("App is in sync with the snapshot", () => {
+  const component = renderer.create(<App />);
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it("works fine with jest-enzyme", () => {
+  const wrapper = shallow(<App />);
+
+  expect(wrapper).toIncludeText("Learn React");
 });
